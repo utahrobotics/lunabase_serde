653532a164169fa22ef67cac21a30c33247c5c94
Manglemix <shabouza030@gmail.com>
2022-03-25 14:00:18 -0600

    Added bool array serde

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::marker::PhantomData;
 use gdnative::prelude::*;
 use gdnative::core_types::{ByteArray, Vector3};
 
@@ -11,6 +12,29 @@ impl Serde {
     /// The "constructor" of the class.
     fn new(_owner: &Node) -> Self {
         Serde
+    }
+    fn u8_to_bools(mut num: u8) -> [bool; 8] {
+        let mut out = [false; 8];
+
+        for mut i in 0..8 {
+            i = 7 - i;
+            let factor = 2u8.pow(i);
+            if num >= factor {
+                num -= factor;
+                out[i as usize] = true;
+            }
+        }
+        out
+    }
+    fn bools_to_u8(bools: [bool; 8]) -> u8 {
+        let mut out = 0;
+
+        for (i, v) in bools.iter().enumerate() {
+            if *v {
+                out += 2u8.pow(i as u32);
+            }
+        }
+        out
     }
 }
 
@@ -56,6 +80,47 @@ impl Serde {
         ].concat())
     }
     #[export]
+    fn serialize_quat(&self, _owner: &Node, quat: Quat) -> ByteArray {
+        ByteArray::from_vec([
+            quat.i.to_le_bytes().as_slice(),
+            quat.j.to_le_bytes().as_slice(),
+            quat.k.to_le_bytes().as_slice(),
+            quat.r.to_le_bytes().as_slice()
+        ].concat())
+    }
+    #[export]
+    fn serialize_bool_array(&self, _owner: &Node, arr: VariantArray) -> ByteArray {
+        let size = arr.len();
+        let iterations = size / 8;
+        let extra = size % 8 != 0;
+
+        let mut out = ByteArray::new();
+        out.resize(iterations + extra as i32);
+        for i in 0..iterations {
+            let idx = i * 8;
+            out.set(i, Self::bools_to_u8([
+                arr.get(idx).to_bool(),
+                arr.get(idx + 1).to_bool(),
+                arr.get(idx + 2).to_bool(),
+                arr.get(idx + 3).to_bool(),
+                arr.get(idx + 4).to_bool(),
+                arr.get(idx + 5).to_bool(),
+                arr.get(idx + 6).to_bool(),
+                arr.get(idx + 7).to_bool()
+            ]))
+        }
+
+        if extra {
+            let offset = iterations * 8;
+            let mut bools = [false; 8];
+            for i in 0..(size - offset) {
+                bools[i as usize] = arr.get(i + offset).to_bool();
+            }
+            out.set(iterations, Self::bools_to_u8(bools))
+        }
+        out
+    }
+    #[export]
     fn deserialize_i64(&self, _owner: &Node, bytes: ByteArray) -> i64 {
         i64::from_le_bytes(byte_array(bytes))
     }
@@ -81,6 +146,32 @@ impl Serde {
             f32::from_le_bytes([bytes.get(4), bytes.get(5), bytes.get(6), bytes.get(7)]),
             f32::from_le_bytes([bytes.get(8), bytes.get(9), bytes.get(10), bytes.get(11)])
         )
+    }
+    #[export]
+    fn deserialize_quat(&self, _owner: &Node, bytes: ByteArray) -> Quat {
+        Quat{
+            i: f32::from_le_bytes([bytes.get(0), bytes.get(1), bytes.get(2), bytes.get(3)]),
+            j: f32::from_le_bytes([bytes.get(4), bytes.get(5), bytes.get(6), bytes.get(7)]),
+            k: f32::from_le_bytes([bytes.get(8), bytes.get(9), bytes.get(10), bytes.get(11)]),
+            r: f32::from_le_bytes([bytes.get(12), bytes.get(13), bytes.get(14), bytes.get(15)]),
+            _unit: PhantomData
+        }
+    }
+    #[export]
+    fn deserialize_bool_array(&self, _owner: &Node, bytes: ByteArray, mut expected_size: usize) -> VariantArray<Unique> {
+        let arr = VariantArray::new();
+        arr.resize(expected_size as i32);
+        for i in 0..bytes.len() {
+            let bools = Self::u8_to_bools(bytes.get(i as i32));
+            for j in 0..expected_size {
+                arr.set(i * 8 + j as i32, bools[j]);
+            }
+            if expected_size <= 8 {
+                break
+            }
+            expected_size -= 8;
+        }
+        arr
     }
 }
 
